a16ee1257ba8268bf97d4d14532e89f42dbf71d2
Chason Zeng <zengccs@gmail.com>
2018-07-29 01:39:13 +0800

    initial version

---
+++ test.js
@@ -0,0 +1,21 @@
+
+
+var arr = [1,2,3.45,'zcs',null,true];
+console.log(arr.length);
+arr.push('www');
+console.log(arr.length);
+console.log(arr.indexOf(null));
+var arr1= arr.slice(0,3);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    
+}
+arr.sort();
+arr.reverse();
+console.log(arr.join('__'));
+var name="zcs";
+var name2="asd"
+arr.splice(3,0,name,name2)
+console.log(arr.join('__'));
+
